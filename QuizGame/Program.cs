9d17f73032b55952dd71eefbfba6fe5d47502dc5
Mike331122 <micpel60@wp.pl>
2024-06-14 18:49:37 +0200

    Wszystko zautowamtyzowane

--- QuizGame/Program.cs
+++ QuizGame/Program.cs
@@ -5,12 +5,19 @@ namespace QuizGame
 {
     class Program
     {
+        //path to string który prowadzi do foldera z projektem
+        //fixedpath to ścieżka która ma usunięty początek przez co zostaje wszystko po bin
+        //goodpath to ścieżka prowadzi do folderu z plikami tekstowymi
+        //filesCount to liczba plików tekstowych, czyli ile jest quizów
+        //fileNames to tablica z nazwami quizów
+        //finding Path jest po to aby funkcja z ucinaniem fileNames wykonała się tylko raz
+        //mainScore to wynik ogólny gracza zliczający punkty ze wszytkich quizów
         static string path = Directory.GetCurrentDirectory();
         static string fixedPath = path.Remove(path.IndexOf("bin"), 9);
         static string goodPath = fixedPath + @"Quizquestion\";
         static int filesCount = Directory.GetFiles(goodPath, "*", SearchOption.TopDirectoryOnly).Length;
         static string[] fileNames = Directory.GetFiles(goodPath, "*", SearchOption.TopDirectoryOnly);
-
+        static bool findingPath= true;
         static int mainScore = 0;
 
         static void Main(string[] args)
@@ -21,16 +28,12 @@ namespace QuizGame
         /// <summary>
         /// Główna funkcja, która wybiera i odpala quizy
         /// 
-        /// Aby dodać qnowy quiz należy dodać plik do folderu Quizquestion w strukturze 
+        /// Aby dodać nowy quiz należy dodać plik do folderu Quizquestion w strukturze 
         /// Pytanie ?
         /// 1. Odpowiedż
         /// 2. Odpowiedż
         /// 3. Odpowiedż
         /// >4. Odpowiedż
-        /// 
-        /// case następny numer:
-        ///     Quiz(fileNames[answer - 1])
-        ///     break;
         /// 
         /// </summary>
         static void Asking()
@@ -43,9 +46,18 @@ namespace QuizGame
 
                 Commends.ColorsLine(ConsoleColor.DarkCyan, "Witaj w grze quizowej! Mam przygotowanych parę quizów dla Ciebie.", 'n');
                 
+                //Funkcja ta ma wykonać się tylko raz bo inaczej ucina tekst
+                if(findingPath)
+                {
+                    for(int i = 0;i<filesCount;i++)
+                    {
+                        fileNames[i] = fileNames[i].Remove(0, fileNames[i].IndexOf("Quizquestion") + 13);
+                    }
+                    findingPath = false;
+                }
+
                 for(int i = 0; i < filesCount; i++)
                 {
-                    fileNames[i] = fileNames[i].Remove(0, fileNames[i].IndexOf("Quizquestion") + 13);
                     string spareName = fileNames[i].Replace(".txt", "");
 
                     Commends.ColorsWrite(ConsoleColor.DarkCyan, $"{i + 1}. Quiz o filmie {spareName}.", 'y');
@@ -55,24 +67,14 @@ namespace QuizGame
 
                 int answer = Commends.Checking("Jaką kategorie wybierasz :",1,filesCount,-1);
 
-                switch(answer)
+                if(answer == -1)
                 {
-                    case 1:
-                        Quiz(fileNames[answer - 1]);
-                        break;
-                    case 2:
-                        Quiz(fileNames[answer - 1 ]);
-                        break;
-                    case 3:
-                        Quiz(fileNames[answer - 1]);
-                        break;
-                    case -1:
-                        playing = false;
-                        break;
-                    //Tutaj można dodać komendę case :
-                    
+                    playing = false;
                 }
-
+                else
+                {
+                    Quiz(fileNames[answer - 1]);
+                }
             }
 
 
